247a9f5ce62876ab98412737885b5d29415e4f8d
bushmusi <bushera.mestofa@gmail.com>
2022-06-09 15:47:46 +0300

    Add comment to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
+  # Post index func
   def index
     @index = 'Posts on index def'
   end
-
+  # Post with given id
   def show
     @show = 'Posts on show def'
   end
