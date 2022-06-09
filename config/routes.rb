d8eff5acc2d5140c8e593e3e7f5570a37e8e7aa1
bushmusi <bushera.mestofa@gmail.com>
2022-06-09 15:48:11 +0300

    Refactor code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # routes for users and user posts
   get "/users/:user_id/posts", to: "posts#index"
   get "/users/:user_id/posts/:id", to: "posts#show"
   get "/users", to: "users#index"
