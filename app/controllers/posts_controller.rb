59ffa8cfa7c53cdc625d35109260da603ad8fe6f
bushmusi <bushera.mestofa@gmail.com>
2022-06-09 15:11:22 +0300

    Add api routing

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def index
+    @index = 'Posts on index def'
+  end
+
+  def show
+    @show = 'Posts on show def'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/users/:user_id/posts", to: "posts#index"
+  get "/users/:user_id/posts/:id", to: "posts#show"
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
 end
