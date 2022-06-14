4c512f7cece89f4858215c274dd72c799a82ddb1
bushmusi <bushera.mestofa@gmail.com>
2022-06-14 17:05:37 +0300

    Data processing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user, class_name: 'user', foreign_key: 'author_id'
   belongs_to :post, class_name: 'post', foreign_key: 'post_id'
+
+  # def update_counter(count)
+
+  # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'post', foreign_key: 'author_id'
   has_many :likes, class_name: 'like', foreign_key: 'author_id'
   has_many :comments, class_name: 'comment', foreign_key: 'author_id'
+
+  # def get_post(user_id)
+  #   Post.find_by(author_id: user_id).limit(3)
+  # end
 end
