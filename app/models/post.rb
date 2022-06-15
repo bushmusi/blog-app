db81f010340e29881592aec6ae2ed591217bc884
bushmusi <bushera.mestofa@gmail.com>
2022-06-15 14:04:38 +0300

    Refactor post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
-  belongs_to :users, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User'
 
   def update_counter(count, user_id)
     User.find(user_id).update(posts_counter: count)
