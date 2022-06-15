60c2d9230fce9f6a8730d13e601fee84ae53cb82
bushmusi <bushera.mestofa@gmail.com>
2022-06-15 14:04:08 +0300

    fix bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :users, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
 
   def update_counter(post_id, count)
