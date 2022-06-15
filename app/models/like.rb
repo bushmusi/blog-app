a92f441f0367221b1762b3baf8e34a86115caa5e
bushmusi <bushera.mestofa@gmail.com>
2022-06-15 14:04:21 +0300

    Refactor like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :users, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :posts, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User'
+  belongs_to :posts
 
   def update_likes_counter(post_id, count)
     Post.find(post_id).update(likes_counter: count)
