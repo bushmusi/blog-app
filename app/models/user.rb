30b61da23b15edfeaefc1568acaba907d8eb6a6c
bushmusi <bushera.mestofa@gmail.com>
2022-06-15 10:36:18 +0300

    Add get post method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :likes, class_name: 'like', foreign_key: 'author_id'
   has_many :comments, class_name: 'comment', foreign_key: 'author_id'
 
-  # def get_post(user_id)
-  #   Post.find_by(author_id: user_id).limit(3)
-  # end
+  def get_post(user_id)
+    Post.find_by(author_id: user_id).order(created_at: :desc).limit(3)
+  end
 end
