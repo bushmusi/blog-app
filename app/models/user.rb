ef599300a8654043717857bb410553a7c1e43dcb
bushmusi <bushera.mestofa@gmail.com>
2022-06-17 17:23:59 +0300

    fetch post fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
-  def get_post(user_id)
-    Post.find_by(author_id: user_id).order(created_at: :desc).limit(3)
+  def fetch_posts
+    posts.order('created_at desc').limit(3)
   end
 end
