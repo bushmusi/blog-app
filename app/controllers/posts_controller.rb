5a10c763205b052dbb5c5dfa1ac056b2720840dd
bushmusi <bushera.mestofa@gmail.com>
2022-06-18 21:52:11 +0300

    Add post crud operation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,31 @@ class PostsController < ApplicationController
   def show
     @current_post = current_post
   end
+
+  def new
+    respond_to do |format|
+      format.html { render :new, locals: { post: Post.new } }
+    end
+  end
+
+  def create
+    user = current_user
+    user.posts_counter += 1
+    user.save
+    post = Post.new(post_params)
+    post.author = user
+    if post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_url
+    else
+      flash[:error] = 'Error: Post could not be saved'
+      redirect_to new_user_post_url
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
