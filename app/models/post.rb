f99df07f990ba5bd5ad9c8137c0b7f191d7958b7
bushmusi <bushera.mestofa@gmail.com>
2022-06-18 19:15:02 +0300

    rename method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-  has_many :comments, foreign_key: 'post_id'
-  has_many :likes, foreign_key: 'post_id'
-  belongs_to :author, class_name: 'User'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def update_counter(count, user_id)
     User.find(user_id).update(posts_counter: count)
   end
 
-  def get_comments(post_id)
-    Comment.find_by(post_id: post_id).limit(5)
+  def recent_comments
+    comments.includes(:author).order('created_at desc').limit(5)
   end
 end
