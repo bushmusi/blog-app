8b2d4e9506f6395ba1fd98cb4eef80c08a2f5cfd
bushmusi <bushera.mestofa@gmail.com>
2022-06-18 21:51:59 +0300

    Add like controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,17 @@
+class LikesController < ApplicationController
+  def create
+    user = current_user
+    post = current_post
+    post.likes.where("author_id = #{user.id}").length.positive? &&
+      return
+
+    like = Like.new(author: user, post: post)
+    return unless like.save
+
+    if params[:distination] == 'index'
+      redirect_back_or_to user_posts_url
+    else
+      redirect_back_or_to user_post_url(id: post.id)
+    end
+  end
+end
