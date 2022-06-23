8a5794ad5b6adc88982fdb71bef2310f67d014e5
bushmusi <bushera.mestofa@gmail.com>
2022-06-23 17:22:35 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_10_080219) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_134517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_080219) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password"
+    t.string "email"
+    t.string "reset_password_token"
+    t.string "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
 
   add_foreign_key "comments", "posts", on_delete: :cascade
