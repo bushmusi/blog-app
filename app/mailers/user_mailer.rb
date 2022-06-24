d0618fce34f3b5f1f2c75ef6599ce6e9da180ceb
iLynette <lynetteacholah@icloud.com>
2022-06-24 12:32:58 +0300

    Set user's password instructions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,5 +3,10 @@ class UserMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'users/mailer' # to make sure that your mailer uses the devise views
 
-  
+  def welcome_reset_password_instructions(user)
+    create_reset_password_token(user)
+    mail(to: user.email, subject: 'Welcome to the New Site')
+  end
+
+  private
 end
