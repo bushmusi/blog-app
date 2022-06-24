6106fd3bf40a35cf2d26af0625002b666e7f185d
bushmusi <bushera.mestofa@gmail.com>
2022-06-24 11:58:50 +0300

    Add user mailer preview

---
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# spec/mailers/previews/user_mailer_preview.rb
+
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+
+class UserMailerPreview < ActionMailer::Preview
+  def welcome_reset_password_instructions
+    UserMailer.welcome_reset_password_instructions(User.first)
+  end
+end
