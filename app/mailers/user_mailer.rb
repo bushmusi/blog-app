f08773a706d196b5e350ec7425061d2f31fd568d
iLynette <lynetteacholah@icloud.com>
2022-06-24 12:33:32 +0300

    Create reset password token

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,4 +9,12 @@ class UserMailer < Devise::Mailer
   end
 
   private
+
+  def create_reset_password_token(user)
+    raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
+    @token = raw
+    user.reset_password_token = hashed
+    user.reset_password_sent_at = Time.now.utc
+    user.save
+  end
 end
