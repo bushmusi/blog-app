e4e66857bf7331e6e207a00f042936cfb6dfb2ac
bushmusi <bushera.mestofa@gmail.com>
2022-06-24 14:42:56 +0300

    Add confirmable fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_134517) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_24_112953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_134517) do
     t.string "reset_password_token"
     t.string "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
   end
 
   add_foreign_key "comments", "posts", on_delete: :cascade
